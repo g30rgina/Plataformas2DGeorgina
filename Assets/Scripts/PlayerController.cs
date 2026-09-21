3f0cf65d6234dbace07e8382428e28dd4b76345d
g30rgina <geokuentaknofunciona@gmail.com>
2026-09-21 14:01:15 +0200

    21/9/2026

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class PlayerController : MonoBehaviour
 
+
+public class PlayerController : MonoBehaviour
 
 { 
 
@@ -12,7 +13,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _jumpHeight = 2; 
     
   [SerializeField] private Transform _groundSensor;
+
     private Rigidbody2D _rigidbody2D; 
+
+    private Animator _animator;
 
     private InputAction _moveAction;
 
@@ -20,18 +24,24 @@ public class PlayerController : MonoBehaviour
 
      private InputAction _jumpAction;  
      
+     private InputAction _attackAction; 
 
     [SerializeField] private float _sensorSize = 1; 
     [SerializeField] private LayerMask _groundLayer; 
-
+    [SerializeField] private int _attackDamage = 7;
+    [SerializeField] private Transform _attackHitBox; 
+    [SerializeField] private float _hitBoxRadius = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
    
    
     void Awake()
     {
-         _rigidbody2D = GetComponent <Rigidbody2D>(); 
+         _rigidbody2D = GetComponent <Rigidbody2D>();  
+         _animator = GetComponent <Animator>();  
+
          _moveAction = InputSystem.actions["Move"];  
          _jumpAction = InputSystem.actions["Jump"];  
+         _attackAction = InputSystem.actions["Attack"];  
     }
 
     // Update is called once per frame
@@ -42,18 +52,25 @@ public class PlayerController : MonoBehaviour
         if(_moveInput.x < 0)
         { 
             transform.rotation = Quaternion.Euler(0, 180, 0);
+            _animator.SetBool("IsRunning", true); 
         }
         else if(_moveInput.x > 0) 
         { 
             transform.rotation = Quaternion.Euler(0, 0, 0); 
+            _animator.SetBool("IsRunning", true); 
         }
-
+        else 
+        { 
+            _animator.SetBool("IsRunning", false); 
+        }
 
 
         if(_jumpAction.WasPressedThisFrame() && IsGrounded())
         {  
             Jump();
-        }
+        } 
+
+        _animator.SetBool("IsJumping", !IsGrounded());
     }
 
     void FixedUpdate() 
@@ -66,6 +83,19 @@ public class PlayerController : MonoBehaviour
         _rigidbody2D.AddForce(Vector2.up * Mathf.Sqrt(_jumpHeight * -2 * Physics2D.gravity.y), ForceMode2D.Impulse);  
     }  
    
+    void Attack()
+    { 
+        _animator.SetTrigger("IsAttacking");
+        Collider2D[] colliders2D = Physics2D.OverlapCircleAll(_groundSensor.position, _sensorSize); 
+        foreach (Collider2D enemy in colliders2D)
+        { 
+          if(enemy.gameObject.layer == 7)  
+            { 
+                Mimik enemyScript = enemy.GetComponent<Mimik>();
+                enemyScript.TakeDamage(_attackDamage);
+            }
+        }      
+    }  
 
      bool IsGrounded() 
     {  
@@ -85,6 +115,9 @@ public class PlayerController : MonoBehaviour
      void OnDrawGizmos()
         { 
             Gizmos.color = Color.red; 
-            Gizmos.DrawWireSphere(_groundSensor.position, _sensorSize); 
+            Gizmos.DrawWireSphere(_groundSensor.position, _sensorSize);  
+
+            Gizmos.color = Color.yellow; 
+            Gizmos.DrawWireSphere(_groundSensor.position, _hitBoxRadius);  
         }  
 }  
